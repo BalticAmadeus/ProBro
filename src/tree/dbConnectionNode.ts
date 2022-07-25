b3b26dca2c7b4382ae638f14a0e881093249c825
Paulius <36190136+PauliusKu@users.noreply.github.com>
2022-07-25 13:56:51 +0300

    Revert "Revert "Delete connection and some other fixes around.""

---
+++ src/tree/dbConnectionNode.ts
@@ -0,0 +1,48 @@
+import * as vscode from "vscode";
+import path = require("path");
+import { INode } from "./INode";
+import { IConfig } from "../view/app/model";
+
+export class DbConnectionNode implements INode {
+  public readonly id: string;
+  public readonly config: IConfig;
+
+  constructor(id: string, config: IConfig, private context: vscode.ExtensionContext) {
+    this.id = id;
+    this.config = config;
+  }
+
+  public getTreeItem(): vscode.TreeItem {
+    return {
+      label: this.config.label,
+      collapsibleState: vscode.TreeItemCollapsibleState.None,
+      description: this.config.description,
+      contextValue: "dbConnection",
+      iconPath: path.join(
+        __filename,
+        "..",
+        "..",
+        "..",
+        "resources/icon",
+        "progress_icon.svg"
+      ),
+    };
+  }
+
+  public async getChildren(): Promise<INode[]> {
+    return [];
+  }
+
+  public async deleteConnection(context: vscode.ExtensionContext) {
+    //TODO fix and move to someplace else
+    console.log("deleteConnection", this.config.label);
+
+    let connections = context.globalState.get<{ [id: string]: IConfig }>(`pro-bro.dbconfig`);
+    if (!connections) {
+        connections = {};
+    }
+    delete connections[this.config.id];
+    this.context.globalState.update(`pro-bro.dbconfig`, connections);
+    vscode.commands.executeCommand(`pro-bro.refreshList`);
+  }
+}
