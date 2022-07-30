332d9344277f8f7ac0e785059a8c46ee0681a025
jkniuksta <j.kniuksta@ba.lt>
2022-07-30 15:58:57 +0300

    header column names sorted by field order

--- src/view/app/query.tsx
+++ src/view/app/query.tsx
@@ -56,10 +56,11 @@ function QueryForm({ vscode, tableData, ...props }: IConfigProps) {
             switch (message.command) {
                 case "data":
                     if (message.data.columns.length != columns.length) {
-                        setColumns(message.data.columns);
+                        setColumns(message.data.columns.sort((a, b) => a.order - b.order));
+                        
                     }
                     setRows([...rows, ...message.data.data]);
-                    setLoaded(loaded + message.data.data.length);
+                    setLoaded(loaded + message.data.data.length)
             }
             setIsLoading(false);
         });
