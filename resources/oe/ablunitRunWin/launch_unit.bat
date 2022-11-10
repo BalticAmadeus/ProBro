b21fd2243f93937b092fd2c183174a8b8c5c8ae3
jkniuksta <j.kniuksta@ba.lt>
2022-11-10 18:38:37 +0200

    changed launch configuration to accept -pf argument

--- resources/oe/ablunitRunWin/launch_unit.bat
+++ resources/oe/ablunitRunWin/launch_unit.bat
@@ -1,8 +1,7 @@
 @ECHO OFF
-
 echo Starting tests...
 
-C:\Progress\OpenEdge\bin\prowin.exe -basekey "INI" -ininame propath.ini -pf C:\workspaces\dbs\SportsTest\sportsTest.pf -p ABLUnitCore.p -param  "CFG=./ablunit.json" 
+C:\Progress\OpenEdge\bin\prowin.exe -basekey "INI" -ininame propath.ini -pf %1 -p ABLUnitCore.p -param  "CFG=./ablunit.json" 
 
 findstr /m "</failure>" ..\results.xml > nul
 if %errorlevel%==0 (
