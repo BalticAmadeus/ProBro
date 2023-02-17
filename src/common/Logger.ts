47ca6320d1fe81dba5e2655ffe6547c6a01ce6c6
jkniuksta <j.kniuksta@ba.lt>
2023-02-17 16:42:08 +0200

    fixed logger class

--- src/common/Logger.ts
+++ src/common/Logger.ts
@@ -1,17 +1,14 @@
-import * as vscode from "vscode";
-
 export class Logger{
 
     private readonly doLog: boolean;
 
-    constructor (logPart: string){
-        this.doLog = vscode.workspace.getConfiguration("ProBro").get(`logging.${logPart}`)!;
-        console.log("logger.ts", this.doLog, logPart);
+    constructor (doLog: boolean){
+        this.doLog = doLog;
     }
 
-    log(message: string, additionalData: any): void{
+    log(message: string, additionalData?: any): void{
         if (this.doLog){
-            console.log(message, additionalData);
+            console.log(message, ": ", additionalData);
         }
     }
 }
