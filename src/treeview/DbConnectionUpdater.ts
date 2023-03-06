fbea2ed805d5f73b8bfc103105eb46c7d494c4ac
Matas Lazdinas <m.lazdinas@gmail.com>
2023-03-06 13:37:50 +0200

    Connecting status implemented

--- src/treeview/DbConnectionUpdater.ts
+++ src/treeview/DbConnectionUpdater.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { DatabaseProcessor } from "../db/DatabaseProcessor";
-import { IConfig } from "../view/app/model";
+import { ConnectionStatus, IConfig } from "../view/app/model";
 import { IRefreshCallback, RefreshWithoutCallback } from "./IRefreshCallback";
 
 export class DbConnectionUpdater {
@@ -14,10 +14,13 @@ export class DbConnectionUpdater {
         let connections = context.globalState.get<{ [id: string]: IConfig }>(`pro-bro.dbconfig`);
 
         if (connections && Object.keys(connections).length !== 0) {
-            for (let id of Object.keys(connections)) {   
+            for (let id of Object.keys(connections)) {
+                connections![id].conStatus = ConnectionStatus.Connecting;
+                context.globalState.update(`pro-bro.dbconfig`, connections);
+                refreshCallback.refresh();
                 await DatabaseProcessor.getInstance().getDBVersion(connections[id]).then((data) => {
-                    connections![id].conStatus = data.error ? false : true;
-                    });;
+                    connections![id].conStatus = ("error" in data) ? ConnectionStatus.NotConnected : ConnectionStatus.Connected;
+                });
                 context.globalState.update(`pro-bro.dbconfig`, connections);
                 refreshCallback.refresh();
             }
