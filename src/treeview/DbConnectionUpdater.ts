5c0cdafbc779516171a7f6457123bc14c75260d9
Matas Lazdinas <m.lazdinas@gmail.com>
2023-03-20 12:55:12 +0200

    green connection status on stopped db fixed

--- src/treeview/DbConnectionUpdater.ts
+++ src/treeview/DbConnectionUpdater.ts
@@ -6,6 +6,7 @@ import { IRefreshCallback, RefreshWithoutCallback } from "./IRefreshCallback";
 export class DbConnectionUpdater {
     constructor (){}
     private locked: boolean = false;
+    private allLoaded:boolean = false;
 
     public async updateConnectionStatuses (context: vscode.ExtensionContext) {
         this.updateConnectionStatusesWithRefreshCallback(context, new RefreshWithoutCallback());
@@ -16,23 +17,56 @@ export class DbConnectionUpdater {
             this.locked = true;
             let connections = context.globalState.get<{ [id: string]: IConfig }>(`pro-bro.dbconfig`);
 
-            if (connections && Object.keys(connections).length !== 0) {
-                for (let id of Object.keys(connections)) {
-                    connections![id].conStatus = ConnectionStatus.Connecting;
-                    await context.globalState.update(`pro-bro.dbconfig`, connections).then( () => {
-                        refreshCallback.refresh();
-                    });
-                    await DatabaseProcessor.getInstance().getDBVersion(connections[id]).then((data) => {
-                        connections![id].conStatus = ("error" in data) ? ConnectionStatus.NotConnected : ConnectionStatus.Connected;
-                        context.globalState.update(`pro-bro.dbconfig`, connections);
-                        refreshCallback.refresh();
-                    });
-                }
+            await this.updateStatuses(connections,context,refreshCallback);
+
+            while (!this.checkIfAllLoaded(connections)) {
+                await this.updateStatuses(connections, context, refreshCallback);
+                await new Promise(resolve => setTimeout(resolve, 500));
             }
+
             this.locked = false;
         }
     }
 
+    private async updateStatuses(connections: {[id: string]: IConfig } | undefined, context: vscode.ExtensionContext, refreshCallback: IRefreshCallback){
 
+        if (connections && Object.keys(connections).length !== 0) {
+            for (let id of Object.keys(connections)) {
+                connections![id].conStatus = ConnectionStatus.Connecting;
+                await context.globalState.update(`pro-bro.dbconfig`, connections).then( () => {
+                    refreshCallback.refresh();
+                });
+                try{
+                    await DatabaseProcessor.getInstance().getDBVersion(connections[id]).then((data) => {
+                        if (data instanceof Error) {
+                            throw new Error("Processor is busy!");
+                        }
+                        else{
+                            connections![id].conStatus = ("error" in data) ? ConnectionStatus.NotConnected : ConnectionStatus.Connected;
+                            console.log("DATACONN", data);
+                            context.globalState.update(`pro-bro.dbconfig`, connections);
+                            refreshCallback.refresh();
+                        }
+                    });
+                }
+                catch{
+                    break;
+                }
+            }
+        }
+    }
+
+    private checkIfAllLoaded(connections: {[id: string]: IConfig } | undefined): boolean{
+        this.allLoaded = true;
+        if (connections && Object.keys(connections).length !== 0) {
+            for (let id of Object.keys(connections)) {
+                if (connections![id].conStatus === ConnectionStatus.Connecting){
+                    this.allLoaded = false;
+                    break;
+                }
+            }
+        }
+        return this.allLoaded;
+    }
 
 }
