5430afb4b99c28cc9422f6009554b592afb25d63
PauliusKu <p.kuprevicius@ba.lt>
2023-04-14 14:18:57 +0300

    fixes

--- src/treeview/DbConnectionUpdater.ts
+++ src/treeview/DbConnectionUpdater.ts
@@ -1,21 +1,21 @@
 import * as vscode from "vscode";
 import { DatabaseProcessor } from "../db/DatabaseProcessor";
 import { ConnectionStatus, IConfig } from "../view/app/model";
+import { IRefreshCallback } from "./IRefreshCallback";
 
 export class DbConnectionUpdater {
     constructor (){}
     private locked: boolean = false;
     private context: vscode.ExtensionContext = {} as vscode.ExtensionContext;
+    private callback : IRefreshCallback = {} as IRefreshCallback;
 
-    public async updateConnectionStatuses (context: vscode.ExtensionContext) {
-        this.updateConnectionStatusesWithRefreshCallback(context);
-    }
-
-    public async updateConnectionStatusesWithRefreshCallback (context: vscode.ExtensionContext) {
+    public async updateConnectionStatusesWithRefreshCallback (context: vscode.ExtensionContext, callback: IRefreshCallback) {
+        this.context = context;
+        this.callback = callback;
+        
         try{
             if (this.locked === false){
                 this.locked = true;
-                this.context = context;
                 await this.updateStatuses();
             }
         } 
@@ -50,6 +50,7 @@ export class DbConnectionUpdater {
 
     private updateStatus(connections: {[id: string]: IConfig }){
         this.context.globalState.update(`pro-bro.dbconfig`, connections);
+        this.callback.refresh();
     }
 
     private wait(): Promise<void> {
---
+++ src/treeview/IRefreshCallback.ts
@@ -0,0 +1,3 @@
+export interface IRefreshCallback {
+    refresh(): void;
+}
