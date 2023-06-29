a0d4d84cd12c9e750dcdf27c318e7800680c1ffc
PauliusKu <p.kuprevicius@ba.lt>
2023-06-29 11:51:02 +0300

    added timeout logic

--- src/db/DatabaseProcessor.ts
+++ src/db/DatabaseProcessor.ts
@@ -6,8 +6,9 @@ import getOEClient from "./OeClient";
 import { Logger } from "../common/Logger";
 
 export class DatabaseProcessor implements IProcessor {
-  private static instance: DatabaseProcessor;
-  private static isProcessRunning: boolean = false;
+  private static instance: DatabaseProcessor; // singleton
+  private static processStartTime: number | undefined = undefined; // undefined means process is not running
+  private static processTimeout: number = 5000; // 5 seconds
   private static errObj = { errMessage: "", isError: false };
 
   private readonly configuration = vscode.workspace.getConfiguration("ProBro");
@@ -21,66 +22,6 @@ export class DatabaseProcessor implements IProcessor {
     }
 
     return DatabaseProcessor.instance;
-  }
-
-  public execShell(params: IOEParams): Promise<any> {
-    const cmd = `${Buffer.from(JSON.stringify(params)).toString("base64")}`;
-    if (DatabaseProcessor.isProcessRunning) {
-      vscode.window.showInformationMessage("Processor is busy");
-      return Promise.resolve(new Error("Processor is busy"));
-    }
-
-    if (DatabaseProcessor.errObj.isError) {
-      vscode.window.showErrorMessage(DatabaseProcessor.errObj.errMessage);
-      return Promise.resolve(new Error(DatabaseProcessor.errObj.errMessage));
-    }
-
-    DatabaseProcessor.isProcessRunning = true;
-    this.logger.log("execShell params", params);
-    var timeInMs = Date.now();
-
-    return getOEClient()
-      .then((client) => {
-        console.log(cmd);
-        return client.sendCommand(cmd);
-      })
-      .then((data) => {
-        var json = JSON.parse(data);
-        console.log(
-          `Process time: ${Date.now() - timeInMs}, OE time: ${
-            json.debug.time
-          }, Connect time: ${json.debug.timeConnect}`
-        );
-        console.log(json.debug);
-        this.logger.log("getOEClient returns", json);
-        DatabaseProcessor.isProcessRunning = false;
-        return json;
-      })
-      .catch((err) => {
-        this.logger.log("getOeClient error", err.message);
-        vscode.window.showWarningMessage(err.message);
-        DatabaseProcessor.errObj.isError = true;
-        DatabaseProcessor.errObj.errMessage = err.message;
-        DatabaseProcessor.isProcessRunning = false;
-        return Promise.resolve(new Error(err.message));
-      });
-  }
-
-  private getConnectionString(config: IConfig) {
-    if (!config.params.includes("-ct")) {
-      if (config.params === "") {
-        config.params += "-ct 1";
-      } else {
-        config.params += " -ct 1";
-      }
-    }
-
-    var connectionString = `-db ${config.name} ${
-      config.user ? "-U " + config.user : ""
-    } ${config.password ? "-P " + config.password : ""} ${
-      config.host ? "-H " + config.host : ""
-    } ${config.port ? "-S " + config.port : ""} ${config.params}`;
-    return connectionString;
   }
 
   public getDBVersion(config: IConfig): Promise<any> {
@@ -147,5 +88,72 @@ export class DatabaseProcessor implements IProcessor {
     } else {
       return Promise.resolve({ fields: [], indexes: [] });
     }
+  }
+
+  private execShell(params: IOEParams): Promise<any> {
+    const cmd = `${Buffer.from(JSON.stringify(params)).toString("base64")}`;
+    if (
+      // if process is running and not timed out
+      DatabaseProcessor.processStartTime !== undefined &&
+      DatabaseProcessor.processStartTime + DatabaseProcessor.processTimeout >
+        Date.now()
+    ) {
+      // then return error
+      vscode.window.showInformationMessage("Processor is busy");
+      return Promise.resolve(new Error("Processor is busy"));
+    }
+
+    if (DatabaseProcessor.errObj.isError) {
+      //
+      vscode.window.showErrorMessage(DatabaseProcessor.errObj.errMessage);
+      return Promise.resolve(new Error(DatabaseProcessor.errObj.errMessage));
+    }
+
+    DatabaseProcessor.processStartTime = Date.now();
+    this.logger.log("execShell params", params);
+    var timeInMs = Date.now();
+
+    return getOEClient()
+      .then((client) => {
+        console.log(cmd);
+        return client.sendCommand(cmd);
+      })
+      .then((data) => {
+        var json = JSON.parse(data);
+        console.log(
+          `Process time: ${Date.now() - timeInMs}, OE time: ${
+            json.debug.time
+          }, Connect time: ${json.debug.timeConnect}`
+        );
+        console.log(json.debug);
+        this.logger.log("getOEClient returns", json);
+        DatabaseProcessor.processStartTime = undefined;
+        return json;
+      })
+      .catch((err) => {
+        this.logger.log("getOeClient error", err.message);
+        vscode.window.showWarningMessage(err.message);
+        DatabaseProcessor.errObj.isError = true;
+        DatabaseProcessor.errObj.errMessage = err.message;
+        DatabaseProcessor.processStartTime = undefined;
+        return Promise.resolve(new Error(err.message));
+      });
+  }
+
+  private getConnectionString(config: IConfig) {
+    if (!config.params.includes("-ct")) {
+      if (config.params === "") {
+        config.params += "-ct 1";
+      } else {
+        config.params += " -ct 1";
+      }
+    }
+
+    var connectionString = `-db ${config.name} ${
+      config.user ? "-U " + config.user : ""
+    } ${config.password ? "-P " + config.password : ""} ${
+      config.host ? "-H " + config.host : ""
+    } ${config.port ? "-S " + config.port : ""} ${config.params}`;
+    return connectionString;
   }
 }
