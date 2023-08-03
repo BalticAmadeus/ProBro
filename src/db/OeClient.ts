3f6f1cd92b1c30150e09f002e288aa74e63be61e
PauliusKu <p.kuprevicius@ba.lt>
2023-08-03 11:05:02 +0300

    fix2

--- src/db/OeClient.ts
+++ src/db/OeClient.ts
@@ -58,16 +58,17 @@ class OEClient {
 
   private runProc(): Promise<any> {
     return new Promise((resolve) => {
-      const accessScript = `chmod +x ${path.join(
-        Constants.context.extensionPath,
-        "resources",
-        "oe",
-        "scripts",
-        "oe.sh"
-      )}`;
-      console.log(accessScript);
-      cp.execSync(accessScript);
       if (process.platform === "linux") {
+        const accessScript = `chmod +x ${path.join(
+          Constants.context.extensionPath,
+          "resources",
+          "oe",
+          "scripts",
+          "oe.sh"
+        )}`;
+        console.log(accessScript);
+        cp.execSync(accessScript);
+
         this.createPfFile();
         this.proc = cp.spawn("bash", [
           "-c",
