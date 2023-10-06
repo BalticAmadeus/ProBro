0c9f2ea6934676db754e1a0fba15d38db03c2ff5
Einingis <mintautas.e@gmail.com>
2023-10-06 12:25:49 +0300

    open multiple databases at the same time

--- src/treeview/TableNode.ts
+++ src/treeview/TableNode.ts
@@ -3,31 +3,38 @@ import { TableDetails } from "../view/app/model";
 import { INode } from "./INode";
 
 export class TableNode implements INode {
-    public tableName: string;
-    public tableType: string;
-    public connectionName: string;
-    public cache: TableDetails | undefined;
+  public tableName: string;
+  public tableType: string;
+  public connectionName: string;
+  public connectionLabel: string;
+  public cache: TableDetails | undefined;
 
-    constructor(private context: vscode.ExtensionContext, tableName: string, tableType: string, connectionName: string) {
-        this.tableName = tableName;
-        this.tableType = tableType;
-        this.connectionName = connectionName;
-    }
+  constructor(
+    private context: vscode.ExtensionContext,
+    tableName: string,
+    tableType: string,
+    connectionName: string,
+    connectionLabel: string
+  ) {
+    this.tableName = tableName;
+    this.tableType = tableType;
+    this.connectionName = connectionName;
+    this.connectionLabel = connectionLabel;
+  }
 
-    public getTreeItem(): vscode.TreeItem {
-        return {
-            label: this.tableName,
-            collapsibleState: vscode.TreeItemCollapsibleState.None,
-            contextValue: "table",
-        };
-    }
+  public getTreeItem(): vscode.TreeItem {
+    return {
+      label: this.tableName,
+      collapsibleState: vscode.TreeItemCollapsibleState.None,
+      contextValue: "table",
+    };
+  }
 
-    public async getChildren(): Promise<INode[]> {
-        return [];
-    }
+  public async getChildren(): Promise<INode[]> {
+    return [];
+  }
 
-    public getFullName(): string { 
-        return this.connectionName + "." + this.tableName;
-    }
-
+  public getFullName(): string {
+    return this.connectionLabel + "." + this.tableName;
+  }
 }
