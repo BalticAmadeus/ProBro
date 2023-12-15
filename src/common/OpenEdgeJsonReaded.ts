85d00ccf40edeaef7856ac96cfed7857e55f73be
Manteliooo <49510463+mmarkauskas@users.noreply.github.com>
2023-12-15 10:43:56 +0200

    Update OpenEdgeJsonReaded.ts

--- src/common/OpenEdgeJsonReaded.ts
+++ src/common/OpenEdgeJsonReaded.ts
@@ -21,6 +21,7 @@ export function parseOEFile(fileContent: string, filePath: string) {
   let num = 0;
 
   const directoryPath = path.dirname(filePath);
+
   dbConnections.forEach((connection: { name: any; connect: any }) => {
     const { name, connect } = connection;
 
