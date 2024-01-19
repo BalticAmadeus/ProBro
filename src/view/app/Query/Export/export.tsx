0a5a4a8398aa0ac72a1220793bdd4478e5b25fcb
jkniuksta <j.kniuksta@ba.lt>
2024-01-19 13:32:13 +0200

    formatted document

--- src/view/app/Query/Export/export.tsx
+++ src/view/app/Query/Export/export.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import Popup from "reactjs-popup";
-import exportFromJSON from "export-from-json";
-import { CommandAction, DataToExport, ICommand } from "../../model";
-import ExportIcon from "@mui/icons-material/FileDownloadTwoTone";
-import "./export.css";
-import { ProBroButton } from "../../assets/button";
-import { Logger } from "../../../../common/Logger";
+import * as React from 'react';
+import Popup from 'reactjs-popup';
+import exportFromJSON from 'export-from-json';
+import { CommandAction, DataToExport, ICommand } from '../../model';
+import ExportIcon from '@mui/icons-material/FileDownloadTwoTone';
+import './export.css';
+import { ProBroButton } from '../../assets/button';
+import { Logger } from '../../../../common/Logger';
 
 export default function ExportPopup({
     wherePhrase,
@@ -13,9 +13,9 @@ export default function ExportPopup({
     sortColumns,
     filters,
     selectedRows,
-  logValue
+    logValue,
 }) {
-  const [exportFormat, setExportFormat] = React.useState("dumpFile");
+    const [exportFormat, setExportFormat] = React.useState('dumpFile');
     const [radioSelection, setRadioSelection] = React.useState(
         Object.keys(DataToExport).filter((key) => Number.isNaN(+key))[0]
     );
@@ -23,17 +23,19 @@ export default function ExportPopup({
     const [isSaving, setIsSaving] = React.useState(false);
     const logger = new Logger(logValue);
 
-  function handleChange({ currentTarget }: React.ChangeEvent<HTMLInputElement>) {
+    function handleChange({
+        currentTarget,
+    }: React.ChangeEvent<HTMLInputElement>) {
         setRadioSelection(currentTarget.value);
         console.log(currentTarget.value);
-  };
+    }
 
-  const exportList = ["dumpFile", "json", "csv", "xls"];
+    const exportList = ['dumpFile', 'json', 'csv', 'xls'];
 
     const getData = () => {
-    console.log("get data");
+        console.log('get data');
         const command: ICommand = {
-      id: "GetData",
+            id: 'GetData',
             action: CommandAction.Export,
         };
         switch (radioSelection) {
@@ -42,7 +44,7 @@ export default function ExportPopup({
                     wherePhrase: wherePhrase,
                     start: 0,
                     pageLength: 100000,
-          lastRowID: "",
+                    lastRowID: '',
                     sortColumns: sortColumns,
                     exportType: exportFormat,
                     timeOut: 0,
@@ -54,7 +56,7 @@ export default function ExportPopup({
                     wherePhrase: wherePhrase,
                     start: 0,
                     pageLength: 100000,
-          lastRowID: "",
+                    lastRowID: '',
                     sortColumns: sortColumns,
                     filters: filters,
                     exportType: exportFormat,
@@ -71,34 +73,34 @@ export default function ExportPopup({
                     wherePhrase: wherePhrase,
                     start: 0,
                     pageLength: 100000,
-          lastRowID: "",
+                    lastRowID: '',
                     sortColumns: sortColumns,
                     filters: filters,
                     exportType: exportFormat,
                     timeOut: 0,
                     crud: rowids,
-          minTime: 0
+                    minTime: 0,
                 };
                 break;
             default:
                 break;
         }
-    logger.log("export request", command);
+        logger.log('export request', command);
         vscode.postMessage(command);
     };
 
     const handleMessage = (event) => {
         const message = event.data;
-    logger.log("got export data", message);
+        logger.log('got export data', message);
         switch (message.command) {
-      case "export":
-        if (message.format === "dumpFile") {
-          logger.log("dumpfile export got.");
+            case 'export':
+                if (message.format === 'dumpFile') {
+                    logger.log('dumpfile export got.');
                     exportFromJSON({
                         data: message.data,
                         fileName: message.tableName,
                         exportType: exportFromJSON.types.txt,
-            extension: "d",
+                        extension: 'd',
                     });
                     setIsSaving(false);
                     break;
@@ -121,10 +123,10 @@ export default function ExportPopup({
     };
 
     React.useEffect(() => {
-    window.addEventListener("message", handleMessage);
+        window.addEventListener('message', handleMessage);
 
         return () => {
-      return window.removeEventListener("message", handleMessage);
+            return window.removeEventListener('message', handleMessage);
         };
     }, []);
 
@@ -143,47 +145,52 @@ export default function ExportPopup({
 
     return (
         <Popup
-      trigger={isWindowSmall ? (
+            trigger={
+                isWindowSmall ? (
                     <ProBroButton startIcon={<ExportIcon />} />
                 ) : (
-        <ProBroButton
-          startIcon={<ExportIcon />}
-        >
+                    <ProBroButton startIcon={<ExportIcon />}>
                         Export
                     </ProBroButton>
                 )
-
             }
             modal
         >
             {(close) => (
-        <div className="modal">
-          <div className="header"> Export to {exportFormat} </div>
-          <div className="content">
+                <div className='modal'>
+                    <div className='header'> Export to {exportFormat} </div>
+                    <div className='content'>
                         <b>Select export format:</b>
                         <br />
                         <br />
                         <select
-              id="dropdown"
-              onChange={(val) => setExportFormat(val.target.value)}
+                            id='dropdown'
+                            onChange={(val) =>
+                                setExportFormat(val.target.value)
+                            }
                             value={exportFormat}
                         >
                             {exportList.map((val) => (
-                <option key={val} value={val}>{val}</option>
+                                <option key={val} value={val}>
+                                    {val}
+                                </option>
                             ))}
                         </select>
                         <br />
 
-            <div className="checkbox">
-              <label><b>
-                Data to export:
-              </b></label>
+                        <div className='checkbox'>
+                            <label>
+                                <b>Data to export:</b>
+                            </label>
                             <br />
                             <br />
-              {Object.keys(DataToExport).filter(key => Number.isNaN(+key)).map((key) => (
-                <label className="radioBtn" key={key}>
-                  <input type="radio"
-                    name="exportdata"
+                            {Object.keys(DataToExport)
+                                .filter((key) => Number.isNaN(+key))
+                                .map((key) => (
+                                    <label className='radioBtn' key={key}>
+                                        <input
+                                            type='radio'
+                                            name='exportdata'
                                             onChange={(e) => handleChange(e)}
                                             value={key}
                                             checked={radioSelection === key}
@@ -193,9 +200,9 @@ export default function ExportPopup({
                                 ))}
                         </div>
                     </div>
-          <div className="btn-container">
+                    <div className='btn-container'>
                         <ProBroButton
-              className="button"
+                            className='button'
                             onClick={() => {
                                 setIsSaving(true);
                                 getData();
@@ -204,12 +211,15 @@ export default function ExportPopup({
                         >
                             Export
                         </ProBroButton>
-            <ProBroButton className="button" onClick={() => close()}>
+                        <ProBroButton
+                            className='button'
+                            onClick={() => close()}
+                        >
                             Cancel
                         </ProBroButton>
                     </div>
                     {isSaving ? (
-            <span className="export-saving">Saving...</span>
+                        <span className='export-saving'>Saving...</span>
                     ) : null}
                 </div>
             )}
