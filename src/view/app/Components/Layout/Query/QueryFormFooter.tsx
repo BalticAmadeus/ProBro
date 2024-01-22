ab7785d0dbb223b4b2425a48f6c31693d1dafd04
mmarkauskas <m.markauskas@ba.lt>
2024-01-22 12:46:38 +0200

    Revert "fix nested paragraph"
    This reverts commit b3f866145adf7914f1a96b8a150a90356ae310e5.

--- src/view/app/Components/Layout/Query/QueryFormFooter.tsx
+++ src/view/app/Components/Layout/Query/QueryFormFooter.tsx
@@ -79,15 +79,12 @@ const QueryFormFooter: React.FC<QueryFormFooterProps> = ({
             >
                 <Grid item>
                     <MonospaceTypography>
-                        Records in grid:
-                        <span
-                            style={{
-                                color: recordColor,
-                                display: 'inline-block',
-                            }}
+                        Records in grid:{' '}
+                        <MonospaceTypography
+                            sx={{ color: recordColor, display: 'inline-block' }}
                         >
                             {totalRecords}
-                        </span>
+                        </MonospaceTypography>
                     </MonospaceTypography>
                 </Grid>
                 {showRecentNumbers ? null : (
