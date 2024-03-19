cec988e92b0ea36e70fb8f402bb74e4748e62616
mmarkauskas <m.markauskas@ba.lt>
2024-03-19 10:52:50 +0200

    change

--- src/view/app/Indexes/index.tsx
+++ src/view/app/Indexes/index.tsx
@@ -1,6 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import './indexes.css';
 import Indexes from './indexes';
+import { ISettings } from '@src/common/IExtensionSettings';
+
+declare global {
+    interface Window {
+        acquireVsCodeApi(): any;
+        configuration: ISettings;
+    }
+}
 
 const root = createRoot(document.getElementById('root'));
 root.render(<Indexes />);
