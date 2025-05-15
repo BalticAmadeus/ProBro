0535bcf94b840663e520e0f3411d195f8db3febc
Erik.Klusovskij <erik.klusovskij@liminorgroup.com>
2025-05-15 14:36:13 +0300

    fixing compilation issues

--- src/repo/client/AClient.ts
+++ src/repo/client/AClient.ts
@@ -33,7 +33,7 @@ export class AClient {
 
             this.client.on('data', (chunk) => {
                 console.log('V2: Data received from the server');
-                const a = decode(chunk, 'windows-1252').replace(/\x00+$/, '');
+                const a = decode(chunk, 'windows-1252').replace(/[\x00]+$/, '');
 
                 this.data += a;
                 if (this.data.endsWith('\n')) {
